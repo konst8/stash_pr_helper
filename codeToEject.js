eb964a4f01479551213b23646ba688352504148d
Konstantin Mishur <nebakraj@gmail.com>
2017-06-29 14:06:09 +0300

    Use ajax request listener instead of timeOut

--- codeToEject.js
+++ codeToEject.js
@@ -1,13 +1,29 @@
 (function($){
   var $reviewersInput = $('.pull-request-reviewers .select2-input');
+  var $suggestedResult = $('.select2-highlighted .select2-result-label'); 
   var time = 0;
-  $.each(reviewersList, function(index, value){
-    setTimeout(function(){
-      $reviewersInput.val(reviewersList[index]).click();
-    }, time);
-    setTimeout(function(){
+  var index = 0;
+
+  // $.each(reviewersList, function(index, value){
+  //   setTimeout(function(){
+  //     $reviewersInput.val(reviewersList[index]).click();
+  //   }, time);
+  //   setTimeout(function(){
+  //     $suggestedResult.trigger('mouseup');
+  //   }, time + 2000);
+  //   time += 2000;
+  // });
+
+  $reviewersInput.val(reviewersList[index]).click();
+
+  $( document ).ajaxComplete(function( event, xhr, settings ) {
+    if ($.parseJSON(xhr.responseText).values[0].name === reviewersList[index]) {
       $('.select2-highlighted .select2-result-label').trigger('mouseup');
-    }, time + 2000);
-    time += 2000;
+      if (index < reviewersList.length) {
+        index += 1;
+        $reviewersInput.val(reviewersList[index]).click();
+      }
+    }
   });
+
 })(jQuery);
