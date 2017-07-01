38e1de2d4fbe71671f76630c482a3e0d9e865019
Konstantin Mishur <nebakraj@gmail.com>
2017-07-01 11:21:59 +0300

    Suggest test reviewers groups on arrow keypress

--- addReviewersScript.js
+++ addReviewersScript.js
@@ -1,4 +1,4 @@
-chrome.storage.sync.get('reviewers', insertReviewers);
+// chrome.storage.sync.get('reviewers', insertReviewers);
 
 function insertReviewers(chromeStorage) {
   var reviewersList = chromeStorage.reviewers.split(', ');
@@ -18,4 +18,44 @@ function insertReviewers(chromeStorage) {
   };
   ajaxRequest.open("GET", embeddedScriptUrl, true);
   ajaxRequest.send();
+}
+
+function addGroupsSuggestions() {
+  var groups = $('<select/>', {
+      'class': 'suggested-revievers-groups',
+      'html': [
+          $('<option/>', {
+              'text': 'test group 1',
+          })
+              .attr({'data-revievers': 'test1, test2, test3'}),
+          $('<option/>', {
+              'text': 'test group 2' 
+          })]
+      })
+          .attr({'size': 2})
+          .on('blur', function(){
+              $('.suggested-revievers-groups').hide();
+          });
+
+  $('.select2-search-field .select2-input')
+      .on('keyup', function(ev){
+          var arrowsKeyCodes = [37, 38, 39, 40];
+          if (arrowsKeyCodes.indexOf(ev.which) != -1 && this.value == '') {
+              $(this).parents('.select2-container').append(groups);
+              hideDefaultDropdown();
+              $('.suggested-revievers-groups')
+                  .show()
+                  .focus();
+          }
+      });
+  }
+
+function hideDefaultDropdown() {
+    $("#select2-drop-mask").hide();
+    $("#select2-drop")
+        .hide()
+        .removeAttr("id");
+    $('.select2-container-active')
+        .removeClass("select2-dropdown-open")
+        .removeClass("select2-container-active");
 }
